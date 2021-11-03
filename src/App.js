d3f11224e08766c94aa20653443e5b912b517324
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-03 16:59:09 +0300

    game menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Menu from './components/Menu';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Menu from "./components/Menu";
+import "./App.css";
 
- const App = () => {
-    return (
-      <>
-        <Menu />
-      </>
-    )
-}
+const App = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Menu} />
+        {/* <Route exact path="/instructions" component={} /> */}
+        {/* <Route exact path="/gamelevel" component={} /> */}
+        {/* <Route exact path="/gameeasy" component={} /> */}
+        {/* <Route exact path="/gamenormal" component={} /> */}
+        {/* <Route exact path="/gamehard" component={} /> */}
+        {/* <Route exact path="/finishmenu" component={} /> */}
+        {/* <Route exact path="*" component={NotFound} /> */}
+      </Switch>
+    </>
+  );
+};
 
 export default App;
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 const Menu = () => {
   return (
     <div className="menuWrapper">
       <div className="menuTitle">
-          <h1>Flip Card</h1>
+        <h1>Flip Card</h1>
       </div>
       <div className="menuButtons">
-        <button>START</button>
-        <button>INSTRACTIONS</button>
+        <button><Link to="/gamelevel">START</Link></button>
+        <button><Link to="/instructions">INSTRACTIONS</Link></button>
       </div>
     </div>
   );
