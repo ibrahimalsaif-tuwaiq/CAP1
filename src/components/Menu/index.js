1959d5e2581a53416288fb3acf7f3803465b18b3
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-03 16:04:10 +0300

    game menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+const Menu = () => {
+  return (
+    <div className="menuWrapper">
+      <div className="menuTitle">
+          <h1>Flip Card</h1>
+      </div>
+      <div className="menuButtons">
+        <button>START</button>
+        <button>INSTRACTIONS</button>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
